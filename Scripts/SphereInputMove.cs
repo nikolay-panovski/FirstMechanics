7acb6928e328b1bd44041c4087f56d8dd44ff6c8
NikolayP <nikipanovski@gmail.com>
2024-02-08 11:50:44 +0100

    Add initial velocity and gravity based on peak height and time
    This does not work "out of the box" with these calculations, because gravity overpowers initial velocity especially in time = (0, 1). Committing in order to try with distance next.

--- Scripts/SphereInputMove.cs
+++ Scripts/SphereInputMove.cs
@@ -3,13 +3,28 @@ using System;
 
 /// Based on tutorial at:
 /// https://docs.godotengine.org/en/3.5/getting_started/first_3d_game/03.player_movement_code.html
+/// 
+/// https://www.youtube.com/watch?v=hG9SzQxaCm8
 public class SphereInputMove : KinematicBody
 {
-    // Declare member variables here.
     [Export] private int speed = 10;
-    [Export] private int fallAcceleration = 75;
 
     private Vector3 velocity = Vector3.Zero;
+
+
+    [Export] private float jumpPeakHeight = 10f;    // default to maxHeight = 4 * character height; no exact science behind it
+    [Export] private float jumpPeakTime = 0.5f;
+
+    private float initialVelocityY;
+    private float baseGravity;
+
+    public override void _Ready()
+    {
+        initialVelocityY = 2 * jumpPeakHeight / jumpPeakTime;
+        baseGravity = -2 * jumpPeakHeight / Mathf.Pow(jumpPeakTime, 2);
+        GD.Print(initialVelocityY);
+        GD.Print(baseGravity);
+    }
 
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,17 +50,18 @@ public class SphereInputMove : KinematicBody
         }
         if (IsOnFloor() && Input.IsActionPressed("jump"))
         {
-            velocity.y += fallAcceleration * delta * 16;
+            velocity.y += initialVelocityY * delta;
         }
-        //debug Y acceleration measures
-        //Utils.DebugPrintTimed(30, "Velocity = " + velocity);
-
 
         if (direction != Vector3.Zero) direction = direction.Normalized();
 
-        velocity.x = direction.x * speed;           // !! no delta (because of _PhysicsProcess?)
+        velocity.x = direction.x * speed;
         velocity.z = direction.z * speed;
-        velocity.y -= fallAcceleration * delta;     // !! yes delta??
+        velocity.y += baseGravity * delta;  // leave the sign to the gravity variable
+
+
+        //debug Y acceleration measures
+        Utils.DebugPrintTimed(30, "Velocity = " + velocity);
 
         velocity = MoveAndSlide(velocity, Vector3.Up);
     }
