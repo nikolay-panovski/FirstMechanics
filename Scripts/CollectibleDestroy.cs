04c6c7c8edae3ee6ea99f9b9fc325856445d4eb7
NikolayP <nikipanovski@gmail.com>
2024-02-12 13:12:50 +0100

    Detect collisions with coins only from the player
    Boneheaded approach, checking the other collider's name to match the player's.

--- Scripts/CollectibleDestroy.cs
+++ Scripts/CollectibleDestroy.cs
@@ -4,7 +4,15 @@ public class CollectibleDestroy : Spatial
 {
     public void OnBodyEnteredDestroyImmediately(Node body)
     {
-        GD.Print("Queueing coin for deletion");
-        QueueFree();
+        // the bad way of detecting specific collision, compare to comparing object names (or lesser, CompareTag) in Unity
+        if (body.Name == "PlayerCylinder")
+        {
+            GD.Print("Queueing coin for deletion");
+            QueueFree();
+        }
+        else
+        {
+            GD.Print("Entered body name: " + body.Name + ". You might want to remove this collision and focus on collision with the Player.");
+        }
     }
 }
