4810bd21fb20fb4fbf936220ac283d0c4886cd19
NikolayP <nikipanovski@gmail.com>
2024-02-15 15:43:27 +0100

    Toy around with referencing variables in Inspector ala Unity

--- Scripts/TestHandler.cs
+++ Scripts/TestHandler.cs
@@ -2,6 +2,11 @@ using Godot;
 
 public class TestHandler : Node
 {
+    // This almost gets us close to Unity Inspector assignable variables.
+    // Except that the Node passed still needs to be got from GetNode, without type safety (throws on incorrect type).
+    // Godot does it (in InterpolatedCamera) via directly casting, Object::cast_to<T> (C++).
+    [Export] private NodePath genericGameObjectRef;
+
     public void OnCollectibleBodyEnteredPrint()
     {
         GD.Print("Coin collected - signal received");
